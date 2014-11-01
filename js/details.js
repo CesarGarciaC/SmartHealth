f6402dba96411c9cf7334bee0e06bc90e972049d
heidyhb <heidy.hernandezb@pucp.pe>
2014-10-31 21:11:55 -0500

    carrusel

--- js/details.js
+++ js/details.js
@@ -75,6 +75,33 @@ $(document).ready($(function () {
     voicePlay();
   });
 
+  //Carrusel
+  $('.center').slick({
+    centerMode: true,
+    centerPadding: '225px',
+    slidesToShow: 3,  
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
   initUtterance();
 
   window.speechSynthesis.cancel();
